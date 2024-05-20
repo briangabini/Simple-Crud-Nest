f5165d41801c50159b83304e5c3dfcd57e2ee9ff
Brian Gabini <91449132+briangabini@users.noreply.github.com>
2024-05-20 23:45:27 +0800

    refactor: change main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
+// import the root component App from a single-file component.
 import App from './App.vue'
+import './style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
+
